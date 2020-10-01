d3bcdd074857e47a50f547c9de1e3920c61c2b98
quytm2239 <quytm2239@gmail.com>
2020-10-01 07:40:13 +0700

    Check age

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,31 @@ router.get('/', function(req, res) {
   res.sendFile('index.html', { root: __dirname });
 })
 
+router.post('/check-age', function(req, res) {
+  let year = req.body.year
+  if (year == null || year == undefined || year.length == 0) {
+    return res.status(400).send({
+      success: false,
+      error: 'Year is not valid, Try again! You need to be 16+ to use this app!'
+    })
+  }
+  var a = parseInt(year)
+  if (a == null) {
+    return res.status(400).send({
+      success: false,
+      error: 'Year is not valid, Try again! You need to be 16+ to use this app!'
+    })
+  }
+  if (a > 2004) {
+    return res.status(400).send({
+      success: false,
+      error: 'Year is not valid, Try again! You need to be 16+ to use this app!'
+    })
+  }
+  return res.status(200).send({
+    success: true,
+    error: 'Yeah! Welcome to Stranger Group Chat'
+  })
+})
+
 module.exports = router;
